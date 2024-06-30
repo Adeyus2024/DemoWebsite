db7cc05429ebbf189f39a6d7478e6b6939c98703
Fatimat O. Adebayo <fattysocioscope@yahoo.com>
2024-06-30 21:51:55 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+console.log("Homework on Cars with their Speed \n\ ");
+
+let car1 = "BMW 320d";
+let car2 = "AUDI AS";
+let car3 = "TESLA MODEL 9";
+let car4 = "MERCEDES E220";
+
+topSpeed1 = 295; 
+topSpeed2 = 284;
+topSpeed3 = 184;
+topSpeed4 = 269;
+
+console.log(`Hi Everyone.
+
+We are reviewing the speed of these new cars coming out in 2022:
+
+Our first car is the ${car1}, it has a top speed of ${topSpeed1 * 0.6}mph.
+
+Our second car is the ${car2}, it has a top speed of ${topSpeed2 * 0.6}mph.
+
+Our third car is the ${car3}, it has a top speed of ${topSpeed3}mph.
+
+Our fourth car is the ${car4}, it has a top speed of ${topSpeed4 * 0.6}mph.
+
+the top speed of all our cars combined is ${topSpeed1 + topSpeed2 + topSpeed3 + topSpeed4}mph`)
---
+++ script14.js
@@ -0,0 +1,4 @@
+alert ("Welcome to my First Demo Website. This is my website that incorporates all lessons learnt. Happy viewing");
+console.log ("\n\ This is my site incorporating FUNCTIONS and other lessons learnt. Happy viewing")
+
+
